5ab010dfa90c886f60288b732b52febaf55a5591
AngeldsWang <zjwang0414@gmail.com>
2015-01-12 21:12:06 +0800

    first commit

---
+++ SwiftWeather/SwiftWeather-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftWeather-Bridging-Header.h
+//  SwiftWeather
+//
+//  Created by ZhenjunWang on 1/12/15.
+//  Copyright (c) 2015 angeldswang. All rights reserved.
+//
+
+#ifndef SwiftWeather_SwiftWeather_Bridging_Header_h
+#define SwiftWeather_SwiftWeather_Bridging_Header_h
+
+
+#endif
